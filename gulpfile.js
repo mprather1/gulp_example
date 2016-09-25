f60919c8068b59be2b8ed766808b3dbbe873df4b
Michael Prather <mprather1@gmail.com>
2016-09-25 17:00:28 -0500

    gulp-concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,36 @@
 var gulp = require('gulp');
- jshint = require('gulp-jshint');
- sass = require('gulp-sass');
- 
- gulp.task('default', ['watch']);
- 
- gulp.task('jshint', function(){
+  gutil = require("gulp-util")
+  jshint = require('gulp-jshint');
+  sass = require('gulp-sass');
+  sourcemaps = require("gulp-sourcemaps");
+  concat = require("gulp-concat")
+  
+  gulp.task('default', ['watch']);
+  
+  gulp.task('jshint', function(){
    return gulp.src('source/javascript/**/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'));
- });
- 
- gulp.task('build-css', function(){
+  });
+  
+  gulp.task('build-css', function(){
    return gulp.src('source/scss/**/*.scss')
-    .pipe(sass())
+    .pipe(sourcemaps.init())
+      .pipe(sass())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('public/assets/stylesheets'));
- });
- 
- gulp.task('watch', function(){
-   gulp.watch('source/javascript/**/*.js', ['jshint']);
+  });
+  
+  gulp.task('build-js', function(){
+   return gulp.src('source/javascript/**/*.js')
+    .pipe(sourcemaps.init())
+      .pipe(concat('bundle.js'))
+      .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('public/assets/javascript'));
+  });
+  
+  gulp.task('watch', function(){
+   gulp.watch('source/javascript/**/*.js', ['jshint', 'build-js']);
    gulp.watch('source/scss/**/*.scss', ['build-css']);
- });
+  });
